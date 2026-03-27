17c4c9c27dfd653ed7e42ae897a53562c05ed40a
Maxim <maksimn219@gmail.com>
2026-03-28 00:03:57 +0300

    initial commit

---
+++ lib/fantasy_character_generator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fantasy_character_generator/version"
+
+module FantasyCharacterGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
